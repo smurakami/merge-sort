157773bbc253478b45d582ae6341bb8c528c83e2
s_murakami <s.murakami@conol.jp>
2014-06-30 16:53:41 +0900

    Refactoring

--- main.c
+++ main.c
@@ -30,39 +30,29 @@ int gArray[BUFFER_SIZE] = {
   9,
 };
 
+// ********************
+//     Merge Sort
+// ********************
+
 void merge(int * array, int size){
+  int buffer[BUFFER_SIZE];
   int size_a = size/2;
   int size_b = size - size_a;
-  int buffer[BUFFER_SIZE];
+  int * array_a = array;
+  int * array_b = array + size_a;
   int i_a = 0;
   int i_b = 0;
-  int i = 0;
-  while (i < size) {
-    int a = array[i_a];
-    int b = array[size_a + i_b];
-    if (a < b) {
+
+  for(int i = 0; i < size; i++){
+    int a = array_a[i_a];
+    int b = array_b[i_b];
+    if ((i_a < size_a) && (a < b)) {
       buffer[i] = a;
       i_a++;
-      i++;
-      if (i_a == size_a) break;
     } else {
       buffer[i] = b;
       i_b++;
-      i++;
-      if (i_b == size_b) break;
     }
-  }
-
-  while (i < size) {
-    if (i_a < size_a) {
-      buffer[i] = array[i_a];
-      i_a++;
-    }
-    if (i_b < size_b) {
-      buffer[i] = array[size_a + i_b];
-      i_b++;
-    }
-    i++;
   }
 
   for (int i = 0; i < size; i++){
@@ -76,6 +66,7 @@ void merge_sort(int * array, int size){
   printSpaces((depth) * 2);
   printf("in: ");
   printArray(array, size);
+
   if (size <= 1) {
     return;
   }
@@ -86,7 +77,8 @@ void merge_sort(int * array, int size){
   merge_sort((array + size_a), size_b);
   depth--;
   merge(array, size);
-  printSpaces((depth - 1) * 2);
+
+  printSpaces(depth * 2);
   printf("out: ");
   printArray(array, size);
 }
